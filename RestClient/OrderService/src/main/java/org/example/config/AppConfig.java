cbc622a9a628ec7877136bb40e55c95fe2fff7d4
Utkarsh Chheda <utkchheda27@gmail.com>
2026-07-07 00:01:02 +0530

    fix

--- RestClient/OrderService/src/main/java/org/example/config/AppConfig.java
+++ RestClient/OrderService/src/main/java/org/example/config/AppConfig.java
@@ -2,18 +2,15 @@ package org.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        //timeouts in ms
-        factory.setConnectTimeout(1000);
-        factory.setReadTimeout(5000);
-        return new RestTemplate(factory);
+    public RestClient restClientInstance() {
+        //RestClient is an interface, need to call it's static method create() either non parameterized
+        //or by passing a RestTemplate object
+        return RestClient.create();
     }
 }
