9e07b45f0481dccebde6fd560151ac745be261c1
Utkarsh Chheda <utkchheda27@gmail.com>
2026-07-06 23:57:34 +0530

    RestClient demo

--- RestClient/OrderService/src/main/java/org/example/controller/OrderController.java
+++ RestClient/OrderService/src/main/java/org/example/controller/OrderController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.MalformedURLException;
@@ -16,21 +15,12 @@ import java.net.MalformedURLException;
 public class OrderController {
 
     @Autowired
-    RestClient restClient;
+    RestTemplate restTemplate;
 
     @GetMapping("/{id}")
     public ResponseEntity<String> getOrders(@PathVariable String id) throws MalformedURLException {
         String url = "http://localhost:8082/products/" + id;
-
-        //Fluent API used here, each method exposes the next set of methods to call.
-        // The retrieve() method is used to perform the HTTP GET request and retrieve the response body as a String.
-        String response = restClient
-                .get()
-                .uri(url)
-                .retrieve()
-                .body(String.class);
-
-        System.out.println("Response from Product API called from order service: " + response);
+        String response = restTemplate.getForObject(url, String.class);
         return ResponseEntity.ok(response);
     }
 }
